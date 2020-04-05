7fec097afcb67aa8dd768d02a64c19da293cc264
Bandana <aurelende@protonmail.com>
2020-04-05 15:29:36 -0400

    auto target hit feature
    Signed-off-by: Bandana <aurelende@protonmail.com>
    Signed-off-by: imini <posner.steven@gmail.com>

--- Intersect.Server/Entities/Player.cs
+++ Intersect.Server/Entities/Player.cs
@@ -1012,7 +1012,7 @@ namespace Intersect.Server.Entities
             base.TryAttack(target, projectile, parentSpell, parentItem, projectileDir);
         }
 
-        public override void TryAttack(Entity target)
+        public void TryAttack(Entity target, bool targetOnFocus)
         {
             if (CastTime >= Globals.Timing.TimeMs)
             {
@@ -1026,7 +1026,7 @@ namespace Intersect.Server.Entities
                 return;
             }
 
-            if (!IsFacingTarget(target))
+            if (!targetOnFocus && !IsFacingTarget(target))
             {
                 return;
             }
